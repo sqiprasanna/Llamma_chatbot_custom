f58a3c1660f7a44c6f8924ca089650ba80ec1351
sqiprasanna <sai1999prasanna@gmail.com>
2024-02-28 15:23:02 -0800

    updated api and main to bobi faq

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import openai
 from llama_index import SimpleDirectoryReader
 
 openai.api_key = st.secrets["openai_key"]
-st.header("Chat with the Streamlit docs 💬 📚")
+st.header("Chat with the BobiHealth's FAQ system💬 📚")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Ask me a question about general question related to pregnancy!"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the maternal docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the maternal health and your job is to answer pregnancy related questions. Assume that all questions are related to the Pregnancy related. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
